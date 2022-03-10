c24093ac06e37f349de16d590e33d1318ee8dac2
Ivamberg Silva <ivambergisilva@gmail.com>
2022-03-10 19:51:12 -0300

    Introdução do site

---
+++ script.js
@@ -0,0 +1,9 @@
+// var filmes = ["Vingadores - Ultimato", "Barraca do Beijo", "Irom Man 2"]
+
+// filmes.push(prompt("Digite:"))
+
+// // document.write("<p>" + filmes[2] + "</p>")
+
+// for(var i = 0; i < filmes.length; i++){
+//     document.write("<p>" + filmes[i] + "</p>")
+// }
