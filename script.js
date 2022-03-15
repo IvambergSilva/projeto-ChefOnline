adb8348543bfd3ea8f50de7c6b6b81bfae2d188c
Ivamberg Silva <ivambergisilva@gmail.com>
2022-03-15 20:36:20 -0300

    Atualização do html e do css, além da implementação do JS

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-// var filmes = ["Vingadores - Ultimato", "Barraca do Beijo", "Irom Man 2"]
+    //Primeiro projeto em "media escala" de HTML5, CSS3 e JavaScript
+    //Ivamberg Silva - 21 anos - iniciante em Web Developer
+    //Projeto realizado em março de 2022 - No início da minha caminhada na programação
 
-// filmes.push(prompt("Digite:"))
 
-// // document.write("<p>" + filmes[2] + "</p>")
+    // Visualizar a senha ou não
 
-// for(var i = 0; i < filmes.length; i++){
-//     document.write("<p>" + filmes[i] + "</p>")
-// }
+    var olhinho = document.getElementById('olhoAberto')
+    var senha = document.getElementById('senha')
+
+    olhinho.addEventListener('click', function(){
+        if (senha.getAttribute('type') == 'password'){
+            senha.type = 'text'
+            olhinho.src="imagens/olhofechado.png"
+        } else {   
+            senha.type = 'password'
+            olhinho.src="imagens/olhoaberto.png"
+        }
+    })
+    
+
+    // Campos de email e senha
+
+    var validade = document.querySelector('#validade')
+    var idemail = document.querySelector("#email")    
+    var criar = document.querySelector("#criar")
+
+        
+    function criarConta(){ 
+        
+        var email = idemail.value
+        var senhaValor = senha.value
+
+        if (email == '' || senha == ''){
+            validade.innerHTML = `Preencha todos os campos acima *`
+        } else if(email == "a@a" && senhaValor == "123a"){
+            // location.href = "index1.html"
+            location.href = "https://google.com"
+        } else {
+            console.log('errado')
+        }
+        
+    }
